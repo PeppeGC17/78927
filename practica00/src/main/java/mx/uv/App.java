4fa878cb4a671dc3641561322e90f2592d4ae2f0
PeppeGC17 <pepegarciacalderon7@gmail.com>
2024-03-05 13:04:07 -0600

    practica00

---
+++ practica00/src/main/java/mx/uv/App.java
@@ -0,0 +1,16 @@
+package mx.uv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        get("/", (request, responde)->"hola");
+    
+    }
+}
